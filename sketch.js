0bf544735e14fd92cbae45a73f20836d45c06e11
wakoIniad <168269931+wakoIniad@users.noreply.github.com>
2026-09-12 02:34:38 +0900

    inprogress

--- sketch.js
+++ sketch.js
@@ -58,19 +58,54 @@ function loadScene() {
 }
 
 const images = {
-  title_base: "resources/title-base.png",
+  title_base: "./res/title-base.png",
 };
 
 const audios = {
-  title: "resources/水縹.mp3",
+  title: "./res/水縹.mp3",
 };
 function preload() {
 }
 
+function logging(...$) {
+  //alert(JSON.stringify($));
+  console.log(...$);
+}
+
+const $ = {
+  cover: "COVER",
+  contain: "CONTAIN",
+  fit: "FIT",
+  center: "CENTER"
+};
+
+function inline_logging(...args) {
+  logging(...args);
+  return null;
+}
+
+function locate(img, pivotMode, expandingMode) {
+  const pivot = {
+    [$.center]: [util.CENTER_X, util.CENTER_Y]
+  }?.[pivotMode]() ?? inline_logging("there aren't such a mode",false);
+  const expanding = {
+    [$.cover]: 
+      util.width/util.height < img.width/img.height?
+      [util.height/img.height*img.width, util.height] :
+      [util.width, util.width/img.width * img.height],
+    [$.contain]:util.width/util.height > img.width/img.height?
+      [util.height/img.height*img.width, util.height] :
+      [util.width, util.width/img.width * img.height],
+    [$.fit]: [util.width, util.height]
+  }?.[expandingMode]() ?? inline_logging("there aren't such a mode",false);
+  return [...pivot, ...expanding];
+}
+
 const scenes = {
   title:(clock, data)=>{
-    audios.title.play();
-    image(images.title_base, ...util.CENTER);
+    audios.title.loop();
+    //text("AIUOE", ...util.CENTER);
+    image(images.title_base, ...locate(images.title_base, $.center, $.cover));
   },
   play:(clock, data)=>{
     
@@ -81,18 +116,20 @@ const scenes = {
 };
 
 
-function setup() {
+async function setup() {
   createCanvas(..._get_screen_size());
   _adapt_browser_environment_changing();
   _setting_default_p5setting();
   
   for(const name of Object.keys(audios)) {
     /**This may cause problem when porting: unmatch type */
-    audios[name] = createAudio(name);
+    audios[name] = createAudio(audios[name],
+    () => {},
+    (err) => logging('audio loading failed', err));
   }
   for(const name of Object.keys(images)) {
     /**This may cause problem when porting: unmatch type */
-    images[name] = loadImage(name);
+    images[name] = await loadImage(images[name]);
   }
 }
 
@@ -102,12 +139,14 @@ function draw() {
   
   background(220);
   _setting_default_p5setting();
+  _update_util_constants();
+
   for(const key of __current_state.scene_composition.active_scenes) {
-    try {
+    //try {
       scenes[key]();
-    } catch(e) {
-      _paused= true;
-      alert(JSON.stringify([e.message]));
-    }
+    //} catch(e) {
+    //  _paused= true;
+    //  logging(e);
+    //}
   }
 }
